b3df5f82bcbaa8838f7bf9c7b800aedbc6ec85b0
vikashkr1712 <vikashkr1712@gmail.com>
2026-06-15 15:47:14 +0530

    images

--- frontend/src/data/roomData.js
+++ frontend/src/data/roomData.js
@@ -1,9 +1,13 @@
+import price1Image from '../assets/images/rooms/price_1.jpg'
+import price2Image from '../assets/images/rooms/price_2.jpg'
+import price3Image from '../assets/images/rooms/price_3.jpg'
+
 export const roomData = [
   {
     id: 1,
     title: 'Family Room',
     price: '$3,499',
-    image: '/src/assets/images/rooms/price_1.jpg',
+    image: price1Image,
     amenities: ['1-2 Persons', 'King Size Bed', 'Bathtub', 'Free WiFi'],
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cu impetus admodum...'
   },
@@ -11,7 +15,7 @@ export const roomData = [
     id: 2,
     title: 'Double Room',
     price: '$3,499',
-    image: '/src/assets/images/rooms/price_2.jpg',
+    image: price2Image,
     amenities: ['1-2 Persons', 'King Size Bed', 'Bathtub', 'Free WiFi'],
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cu impetus admodum...'
   },
@@ -19,7 +23,7 @@ export const roomData = [
     id: 3,
     title: 'Family Room',
     price: '$3,499',
-    image: '/src/assets/images/rooms/price_3.jpg',
+    image: price3Image,
     amenities: ['1-2 Persons', 'King Size Bed', 'Bathtub', 'Free WiFi'],
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cu impetus admodum...'
   }
--- frontend/src/data/testimonialData.js
+++ frontend/src/data/testimonialData.js
@@ -1,20 +1,24 @@
+import frame1Image from '../assets/images/testimonials/frame_1.png'
+import frame2Image from '../assets/images/testimonials/frame_2.png'
+import frame3Image from '../assets/images/testimonials/frame_3.png'
+
 export const testimonialData = [
   {
     id: 1,
     name: 'Emily Riddick',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictumst eu, humilis ac....',
-    avatar: '/src/assets/images/testimonials/frame_1.png'
+    avatar: frame1Image
   },
   {
     id: 2,
     name: 'Michael Chen',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictumst eu, humilis ac....',
-    avatar: '/src/assets/images/testimonials/frame_2.png'
+    avatar: frame2Image
   },
   {
     id: 3,
     name: 'Lisa Rodriguez',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictumst eu, humilis ac....',
-    avatar: '/src/assets/images/testimonials/frame_3.png'
+    avatar: frame3Image
   }
 ];
